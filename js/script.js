c7a009c2d8788b005886699bfcce87b741a72e5c
Mariko Kusaka <cerica002001@gmail.com>
2023-07-27 15:45:51 +0900

    0727

--- js/script.js
+++ js/script.js
@@ -7,6 +7,19 @@ $(".hmenu").click(function () {
 });
 
 // Bパターンの設定
-$(".container").click(function () {
+$("body").click(function () {
   $(".hmenu").removeClass("close");
+});
+
+$(".hmenu2").click(function () {
+  // 上下パターン
+  //   $("nav").slideToggle(500);
+  $(this).toggleClass("close");
+  // return false これ以降何もしない
+  return false;
+});
+
+// Bパターンの設定
+$("body").click(function () {
+  $(".hmenu2").removeClass("close");
 });
